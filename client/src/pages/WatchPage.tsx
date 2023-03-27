3da7ccf7f90c4ecbbc055901ba8318792a7fd0c3
n0RE <nore@desktop>
2023-03-27 23:53:20 +0300

    added check if we're on right page

--- client/src/pages/WatchPage.tsx
+++ client/src/pages/WatchPage.tsx
@@ -1,22 +1,33 @@
 import React, { useEffect, useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import { Link, useNavigate, useSearchParams } from 'react-router-dom';
+import { getById } from '../backendAPI/userAPI';
 import { getVideo, getVideosFromUser } from '../backendAPI/videoAPI';
 import { useFetching } from '../hooks/useFetching';
 import '../styles/watchpage.scss'
-import { IVideo } from '../types/Interfaces';
+import { fetchedUser, IVideo } from '../types/Interfaces';
 
 function WatchPage() {
     const [params] = useSearchParams()
+    const navigate = useNavigate()
     const [video, setVideo] = useState<IVideo>()
+    const [user, setUser] = useState<fetchedUser>()
     const videoURL = params.get('v')
 
-    const [fetchVideo] = useFetching(async () => {       
+    const [fetchAll, isFetching, error] = useFetching(async() => {
         const video = await getVideo(Number(videoURL))
         setVideo(video)
+        const user = await getById(video.userId)
+        setUser(user)
     })
 
     useEffect(() => {
-        fetchVideo()
+        if (error !== "") {
+            navigate('/')
+        }
+    }, [isFetching])
+
+    useEffect(() => {
+        fetchAll()        
     }, [])
 
     return (
@@ -28,7 +39,26 @@ function WatchPage() {
                     </video>
                 </div>
                 <div className='watchpage_media'>
-                    
+                    <div className="media_left">
+                        <div className="media_left_w">
+                            <div className="media_meta">
+                                <div className='media_title'>{video?.title}</div>
+                                <div className="media_usermeta">
+                                    <a className='user_avatar'>
+                                        <div className='user_avatar_container'>
+                                            <img src={process.env.REACT_APP_API_URL + `${user?.profileImg}`} />
+                                        </div>
+                                    </a>
+                                    <div className='user_meta'>
+                                        <Link to={`/channel/${video?.userId}`} className='user_username'>{user?.username}</Link>
+                                        <div className='user_subscribers'>{user?.subscribersCount} подписчиков</div>
+                                    </div>
+                                    <button className='user_subscribebtn'>Подписаться</button>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
