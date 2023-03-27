adbe95786c44fece7e3b6601b9a3e5d6d199dc87
n0RE <nore@desktop>
2023-03-27 23:53:40 +0300

    connected element with backend

--- client/src/components/VideoItem/VideoItem.tsx
+++ client/src/components/VideoItem/VideoItem.tsx
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './VideoItem.module.scss'
-import { IVideo } from '../../types/Interfaces';
+import { fetchedUser, IVideo } from '../../types/Interfaces';
 import { Link } from 'react-router-dom';
+import { useFetching } from '../../hooks/useFetching';
+import { getById } from '../../backendAPI/userAPI';
 
 interface VideoItemProps {
     video: IVideo
 }
 
 const VideoItem: React.FC<VideoItemProps> = ({video}) => {
+    const [user, setUser] = useState<fetchedUser>()
+
+    const [fetchUser] = useFetching(async() => {
+        const user = await getById(video.userId)
+        setUser(user)
+    })
+
+    useEffect(() => {
+        fetchUser()
+    }, [])
+
     return (
         <div className={styles.video}>
             <div className={styles.video_w}>
-                <a className={styles.video_preview}>
+                <Link to={`/watch?v=${video.video}`} className={styles.video_preview}>
                     <img src={process.env.REACT_APP_API_URL + `${video.previewImg}`} className={styles.video_previewImg}/>
-                </a>
+                </Link>
                 <div className={styles.video_details}>
                     <a className={styles.video_avatar}>
                         <div className={styles.video_avatar_container}>
-                            <img src="" alt="" />
+                            <img src={process.env.REACT_APP_API_URL + `${user?.profileImg}`} />
                         </div>
                     </a>
                     <div className={styles.video_meta}>
@@ -26,9 +39,9 @@ const VideoItem: React.FC<VideoItemProps> = ({video}) => {
                                 {video.title}
                             </Link>
                         </div>
-                        <Link to={`/channel/${video.userId}`} className={styles.video_meta_username}>username</Link>
+                        <Link to={`/channel/${video.userId}`} className={styles.video_meta_username}>{user?.username}</Link>
                         <div className={styles.video_metadata}>
-                            <span className={styles.video_views}>{video.views} тыс. просмотров</span>
+                            <span className={styles.video_views}>{video.views} просмотров</span>
                             <span className={styles.video_uploadDate}>2 недели назад</span>
                         </div>
                     </div>
