0c33f552bbdbf435d1124351df9584e638432805
n0RE <nore@desktop>
2023-03-28 22:11:31 +0300

    navbar refers to user if user is auth

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useAppSelector } from '../../hooks/useReduxHooks';
 import Searchbar from '../Searchbar/Searchbar';
+import Avatar from '../UI/Avatar/Avatar';
 import Burger from '../UI/Burger/Burger';
 import styles from './Navbar.module.scss'
 
 const Navbar: React.FC = () => {
+    const user = useAppSelector(state => state.userSlice)
     return (
         <div className={styles.navbar}>
             <div className={styles.navbar_logobox}>
@@ -13,7 +16,10 @@ const Navbar: React.FC = () => {
             </div>
             <Searchbar />
             <div className={styles.navbar_auth}>
-                <Link to='/auth' className={styles.navbar_login}>Войти</Link>
+                {user.isAuth
+                    ? <Avatar channelId={user.user.id} profileImg={user.user.profileImg} />
+                    : <Link to='/auth' className={styles.navbar_login}>Войти</Link>
+                }
             </div>
         </div>
     );
