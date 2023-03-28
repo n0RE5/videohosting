866c2a2ee73520945c738842d3b08a3a4a35d7a8
n0RE <nore@desktop>
2023-03-28 22:11:48 +0300

    added searchbar functions

--- client/src/components/Searchbar/Searchbar.tsx
+++ client/src/components/Searchbar/Searchbar.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from './Searchbar.module.scss'
 
 const Searchbar: React.FC = () => {
+    const [searchQuery, setSearchQuery] = useState<string>('')
+    const navigate = useNavigate()
+    
+    const redirect = (e: React.MouseEvent) => {
+        e.preventDefault()
+        navigate(`/search?searchQuery=${searchQuery}`)
+        return
+    }
+
     return (
         <form className={styles.searchbar}>
             <div className={styles.search_input}>
-                <input className={styles.searchbar_input} type="text" placeholder='Введите запрос' required  />
+                <input className={styles.searchbar_input} value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} type="text" placeholder='Введите запрос' required  />
             </div>
-            <button className={styles.searchbar_btn}>S</button>
+            <button onClick={redirect} className={styles.searchbar_btn}>S</button>
         </form>
     );
 };
