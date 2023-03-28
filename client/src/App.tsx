d1a4c95b3965ff02d6a83dfc3a54fa05f5b2f48a
n0RE <nore@desktop>
2023-03-28 23:19:27 +0300

    added primary user loading from token

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,33 @@
+import { useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { check } from './backendAPI/userAPI';
 import AppRouter from './components/AppRouter';
 import Navbar from './components/Navbar/Navbar';
+import Loader from './components/UI/Loader/Loader';
+import { useFetching } from './hooks/useFetching';
+import { useAppDispatch } from './hooks/useReduxHooks';
+import { fetchUser, fetchUserError, fetchUserSuccess } from './store/reducers/UserSlice';
 
 function App() {
+  const dispatch = useAppDispatch()
+  const [checkUser, isUserLoading] = useFetching(async () => {
+    try {
+        dispatch(fetchUser())
+        const response = await check()
+        dispatch(fetchUserSuccess(response))
+    } catch (error: any) {
+        dispatch(fetchUserError(error.response?.data?.message))
+    }
+  })
+
+  useEffect(() => {
+    checkUser()
+  }, [])
+
+  if(isUserLoading) {
+    return <Loader />
+  }
+
   return (
     <div className="App">
       <BrowserRouter>
