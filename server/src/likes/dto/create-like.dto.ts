5796f8681aafee72741604088a350091229b0cf4
n0RE <nore@desktop>
2023-03-28 23:18:48 +0300

    fixed dto

--- server/src/likes/dto/create-like.dto.ts
+++ server/src/likes/dto/create-like.dto.ts
@@ -1,9 +1,6 @@
 import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateLikeDto {
-    @ApiProperty({example: '1', description: "User ID"})
-    readonly userId: number;
-
     @ApiProperty({example: '1', description: "VIDEO ID"})
     readonly videoId: number;
 }
