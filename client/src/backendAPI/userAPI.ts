aa3f3edc478029ee4f34364acf6295a867095291
n0RE <nore@desktop>
2023-03-28 23:19:06 +0300

    added checkUser function

--- client/src/backendAPI/userAPI.ts
+++ client/src/backendAPI/userAPI.ts
@@ -1,4 +1,4 @@
-import { $host } from "./index"
+import { $authHost, $host } from "./index"
 import { userDTO } from "../types/Dto"
 import jwt_decode from "jwt-decode"
 import { fetchedUser, IUser } from "../types/Interfaces"
@@ -13,6 +13,12 @@ export const login = async (dto: userDTO): Promise<IUser> => {
     const response = await $host.post('auth/login', dto)
     localStorage.setItem('token', response.data.token)
     return jwt_decode(response.data.token)
+}
+
+export const check = async (): Promise<IUser> => {
+    const response = await $authHost.get('auth/check')    
+    localStorage.setItem('token', response.data)
+    return jwt_decode(response.data)
 }
 
 export const getById = async (id: number): Promise<fetchedUser> => {
