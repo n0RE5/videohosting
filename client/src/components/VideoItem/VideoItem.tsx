1991acea7b65af22399fab219470d51cf0fb9de9
n0RE <nore@desktop>
2023-03-30 14:19:34 +0300

    added 100% size mode

--- client/src/components/VideoItem/VideoItem.tsx
+++ client/src/components/VideoItem/VideoItem.tsx
@@ -9,9 +9,10 @@ import { parseRawDate, parseViewsToString } from '../../utils/Parsers';
 
 interface VideoItemProps {
     video: IVideo
+    vertical?: boolean
 }
 
-const VideoItem: React.FC<VideoItemProps> = ({video}) => {
+const VideoItem: React.FC<VideoItemProps> = ({video, vertical}) => {
     const [user, setUser] = useState<fetchedUser>()
 
     const fetchUser = async () => {
@@ -24,7 +25,7 @@ const VideoItem: React.FC<VideoItemProps> = ({video}) => {
     }, [])
 
     return (
-        <div className={styles.video}>
+        <div data-vertical={vertical} className={styles.video}>
             <div className={styles.video_w}>
                 <span className={styles.video_preview}>
                     <Link to={`/watch?v=${video.id}`} className={styles.video_preview_w}>
@@ -32,20 +33,34 @@ const VideoItem: React.FC<VideoItemProps> = ({video}) => {
                     </Link>
                 </span>
                 <div className={styles.video_details}>
-                    <div className={styles.video_avatar}>
-                        <Avatar channelId={video.userId} profileImg={user?.profileImg} />
-                    </div>
+                    {vertical 
+                        ? null
+                        : <div className={styles.video_avatar}>
+                            <Avatar channelId={video.userId} profileImg={user?.profileImg} />
+                          </div>
+                    }
                     <div className={styles.video_meta}>
                         <div className={styles.video_meta_title}>
                             <Link to={`/watch?v=${video.id}`}>
                                 {video.title}
                             </Link>
                         </div>
-                        <Link to={`/channel/${video.userId}`} className={styles.video_meta_username}>{user?.username}</Link>
+                        {vertical
+                            ? <div className={styles.video_verticaluser}>
+                                <div className={styles.video_avatar}>
+                                    <Avatar channelId={video.userId} profileImg={user?.profileImg} />
+                                </div>
+                                <Link to={`/channel/${video.userId}`} className={styles.video_meta_username}>{user?.username}</Link>
+                              </div>
+                            : <Link to={`/channel/${video.userId}`} className={styles.video_meta_username}>{user?.username}</Link>
+                        }
                         <div className={styles.video_metadata}>
                             <span className={styles.video_views}>{parseViewsToString(video.views)}</span>
                             <span className={styles.video_uploadDate}>{parseRawDate(video.createdAt)}</span>
                         </div>
+                        {vertical &&
+                             <span className={styles.video_description}>{video.description}</span>
+                        }
                     </div>
                 </div>
             </div>
