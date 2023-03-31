8f32e4ead8656098e46c431176bd16449f121ea9
n0RE <nore@desktop>
2023-03-31 15:45:14 +0300

    updated subspage

--- client/src/pages/SubsPage.tsx
+++ client/src/pages/SubsPage.tsx
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import DefaultContainer from '../components/DefaultContainer/DefaultContainer';
+import { getUserSubs } from '../backendAPI/subscribtionsAPI';
+import { useFetching } from '../hooks/useFetching';
+import { fetchedUser } from '../types/Interfaces';
+import Avatar from '../components/UI/Avatar/Avatar';
+import { parseSubsToString } from '../utils/Parsers';
+import '../styles/subspage.scss'
+import Loader from '../components/UI/Loader/Loader';
 
 function SubsPage() {
+    const [userSubs, setUserSubs] = useState<fetchedUser[]>([])
+
+    const [fetchSubscriptions, isFetching] = useFetching(async () => {
+        const response = await getUserSubs()
+        setUserSubs(response)
+    })
+
+    useEffect(() => {
+       fetchSubscriptions()
+    }, [])
+
+    if(isFetching) {
+        return <Loader />
+    }
+
     return (
         <DefaultContainer appTitle='Подписки - YouVI'>
-            
+            <div className="subspage_w">
+                {userSubs.length
+                    ? <div className='subspage_grid'>
+                        {userSubs.map(sub =>
+                            <div className='subspage_item'>
+                                <Avatar large channelId={sub.id} profileImg={sub.profileImg} />
+                                <div className='subspage_item_username'>{sub.username}</div>
+                                <div className='subspage_item_subs'>{parseSubsToString(Number(sub.subscribersCount))}</div>
+                            </div>
+                        )}
+                    </div>
+                    : <div className='subspage_empty'>Похоже вы еще ни на кого ни подписаны...</div>
+                }
+            </div>
         </DefaultContainer>
     );
 };
