6c49fa03422c979bffa6d4de45f7515c2c92a324
n0RE <nore@desktop>
2023-04-09 18:39:15 +0300

    removed unused imports

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,15 +3,14 @@ import { BrowserRouter } from 'react-router-dom';
 import { check } from './backendAPI/userAPI';
 import AppRouter from './components/AppRouter';
 import Navbar from './components/Navbar/Navbar';
-import Sidebar from './components/Sidebar/Sidebar';
 import Loader from './components/UI/Loader/Loader';
-import { useFetching } from './hooks/useFetching';
-import { useAppDispatch, useAppSelector } from './hooks/useReduxHooks';
+import { useAppDispatch  } from './hooks/useReduxHooks';
 import { fetchUser, fetchUserError, fetchUserSuccess } from './store/reducers/UserSlice';
 
 function App() {
   const dispatch = useAppDispatch()
   const [userLoading, setUserLoading] = useState<boolean>(true)
+  
   const checkUser = async () => {
     try {
         setUserLoading(true)
