0b5cbf540e42a7e0fe3810b3139df73d31cc887c
n0RE <nore@desktop>
2023-04-09 18:40:02 +0300

    fixed useSubscriptions hook

--- client/src/hooks/useSubscriptions.ts
+++ client/src/hooks/useSubscriptions.ts
@@ -1,36 +1,30 @@
-import { useEffect, useState } from "react"
+import { useEffect, useMemo, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { checkSubscription, subscribeToUser, unsubscribeFromUser } from "../backendAPI/subscribtionsAPI"
 import { AUTH_PATH } from "../utils/Consts"
 import { useFetching } from "./useFetching"
 import { useAppSelector } from "./useReduxHooks"
 
-export const useSubscriptions = (userId?: number) => {
+export const useSubscriptions = (userId: number) => {
     const [isSubscribed, setIsSubscribed] = useState<boolean>(false)
     const isAuth = useAppSelector(state => state.userSlice.isAuth)
     const navigate = useNavigate()
 
-    const checkSubscribe = async () => {
-        if(!userId) return
+    const [subscribe, subscribeFetching] = useFetching(async () => {
+        if(subscribeFetching) return
+        if(!isAuth) return navigate(AUTH_PATH)
+
+        if (isSubscribed) {
+            const response = await unsubscribeFromUser({userId})
+        } else {
+            const response = await subscribeToUser({userId})
+        }
+    })
+
+    const checkSubscribe = useMemo(async () => {
         const response = await checkSubscription({userId})
         setIsSubscribed(response)
-    }
+    }, [subscribeFetching, userId])
 
-    const [subscribe, subscribeFetching] = useFetching(async () => {
-        if(!userId) return
-        if(!isAuth) return navigate(AUTH_PATH)
-        const response = await subscribeToUser({userId})
-    })
-
-    const [unsubscribe, unsubscribeFetching] = useFetching(async () => {
-        if(!userId) return
-        if(!isAuth) return navigate(AUTH_PATH)
-        const response = await unsubscribeFromUser({userId})
-    })
-
-    useEffect(() => {
-        checkSubscribe()
-    }, [subscribeFetching, unsubscribeFetching, userId])
-
-    return [isSubscribed, subscribe, unsubscribe] as const
+    return [isSubscribed, subscribe] as const
 }
