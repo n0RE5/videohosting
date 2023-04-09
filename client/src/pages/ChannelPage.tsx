7ec7f0f33bb236641c93d427ed5bfbaf885ce20a
n0RE <nore@desktop>
2023-04-09 18:41:05 +0300

    refactored code

--- client/src/pages/ChannelPage.tsx
+++ client/src/pages/ChannelPage.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { getById } from '../backendAPI/userAPI';
 import { getVideosFromUser } from '../backendAPI/videoAPI';
 import Avatar from '../components/UI/Avatar/Avatar';
@@ -8,23 +8,30 @@ import VideoGridbox from '../components/VideoGridbox/VideoGridbox';
 import { useFetching } from '../hooks/useFetching';
 import { fetchedUser, IVideo } from '../types/Interfaces';
 import { parseSubsToString } from '../utils/Parsers';
-import '../styles/channelpage.scss'
 import { useSubscriptions } from '../hooks/useSubscriptions';
 import { fetchedUserPlacehoder } from '../utils/Placeholders';
 import DefaultContainer from '../components/DefaultContainer/DefaultContainer';
+import '../styles/channelpage.scss'
 
 function ChannelPage () {
     const param = useParams()
-    const [videos, setVideos] = useState<IVideo[]>([])
-    const [videoCount, setVideoCount] = useState<number>(0)
-    const [channelUser, setChannelUser] = useState<fetchedUser>(fetchedUserPlacehoder)
-    const [isSubscribed, subscribe, unsubscribe] = useSubscriptions(Number(param?.userId))
+    const navigate = useNavigate()
 
-    const [fetchAll, isFetching] = useFetching(async() => {
-        const response = await getVideosFromUser({userId: Number(param?.userId), limit: 0, page: 0})
+    const [videos, setVideos] = useState<IVideo[]>([])
+    const [channelUser, setChannelUser] = useState<fetchedUser>(fetchedUserPlacehoder)
+    const [isSubscribed, subscribe] = useSubscriptions(channelUser.id)
+    const subString = parseSubsToString(Number(channelUser.subscribersCount))
+
+    const [fetchAll, isFetching] = useFetching( async() => {
+        const user = await getById(Number(param.userId))
+
+        if(!user) {
+            return navigate('/error')
+        }
+
+        const response = await getVideosFromUser({userId: user.id, limit: 0, page: 0})
+
         setVideos(response.rows)
-        setVideoCount(response.count.length)
-        const user = await getById(Number(param?.userId))
         setChannelUser(user)
     })
 
@@ -41,24 +48,18 @@ function ChannelPage () {
             <div className='channelpage'>
                 <div className="channelpage_w">
                     <div className='channelpage_usermeta'>
-                        <Avatar large channelId={Number(param?.userId)} profileImg={channelUser.profileImg} />
+                        <Avatar large channelId={channelUser.id} profileImg={channelUser.profileImg} />
                         <div className='channelpage_usercontainer'>
                             <div className='channelpage_user'>
                                 <div className='channelpage_username'>@{channelUser.username}</div>
                                 <span className='channelpage_user_media'>
-                                    <span className='channelpage_subs'>{parseSubsToString(Number(channelUser.subscribersCount))}</span>
+                                    <span className='channelpage_subs'>{subString}</span>
                                     &nbsp;&nbsp;
-                                    <span className='channelpage_videoCount'>{videoCount} видео</span>
+                                    <span className='channelpage_videoCount'>{videos.length} видео</span>
                                 </span>
                             </div>
                             <button
-                                onClick={() => {
-                                    if(isSubscribed) {
-                                        unsubscribe()
-                                    } else {
-                                        subscribe()
-                                    }
-                                }}
+                                onClick={subscribe}
                                 data-subscribed={isSubscribed}
                                 className='channelpage_subscribe'
                             >
