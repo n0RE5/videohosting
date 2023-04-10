3291f9205e732d785727a68ad640902998edd700
n0RE <nore@desktop>
2023-04-10 19:31:21 +0300

    updated settings page

--- client/src/pages/StudioPages/StudioSettingsPage.tsx
+++ client/src/pages/StudioPages/StudioSettingsPage.tsx
@@ -1,22 +1,31 @@
 import React, { useState } from 'react';
 import StudioContainer from '../../components/StudioContainer/StudioContainer';
 import ChangePasswordModal from '../../components/ChangePasswordModal/ChangePasswordModal';
+import EditUserModal from '../../components/EditUserModal/EditUserModal';
 
 const StudioSettingsPage: React.FC = () => {
     const [modalActive, setModalActive] = useState<boolean>(false)
+    const [userModalActive, setUserModalActive] = useState<boolean>(false)
 
     return (
         <StudioContainer>
             <div className='studiopage'>
                 <div className='studiopage_w'>
                     <div className='studiopage_title'>Настройки</div>
-                    <div className='studiopage_main_card'>
-                        <div className='studiopage_main_upload'>Здесь вы можете поменять пароль</div>
-                        <a onClick={() => setModalActive(true)} className='studiopage_main_upload_btn'>Изменить пароль</a>
+                    <div className='studiopage_cards'>
+                        <div className='studiopage_main_card'>
+                            <div className='studiopage_main_upload'>Здесь вы можете поменять пароль</div>
+                            <a onClick={() => setModalActive(true)} className='studiopage_main_upload_btn'>Изменить пароль</a>
+                        </div>
+                        <div className='studiopage_main_card'>
+                            <div className='studiopage_main_upload'>Здесь вы можете редактировать профиль</div>
+                            <a onClick={() => setUserModalActive(true)} className='studiopage_main_upload_btn'>Изменить Профиль</a>
+                        </div>
                     </div>
                 </div>
             </div>
             <ChangePasswordModal active={modalActive} setActive={setModalActive} />
+            <EditUserModal active={userModalActive} setActive={setUserModalActive} />
         </StudioContainer>
     );
 };
